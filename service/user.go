050092338e24550458fc971ae2c521acdb78c70e
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-04-30 21:23:40 +0700

    feat: init new instance service

--- service/user.go
+++ service/user.go
@@ -10,6 +10,21 @@ type userService struct {
 	userRepo repository.UserRepository
 }
 
+func (u userService) ReadByID(ctx context.Context, ID int64) (model.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userService) Update(ctx context.Context, ID int64, user model.User) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userService) Delete(ctx context.Context, ID int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (u userService) Create(ctx context.Context, user model.User) (model.User, error) {
 	user, err := u.userRepo.Create(ctx, user)
 	if err != nil {
@@ -17,4 +32,10 @@ func (u userService) Create(ctx context.Context, user model.User) (model.User, e
 	}
 
 	return user, nil
+}
+
+func NewUserService(userRepository repository.UserRepository) UserService {
+	return userService{
+		userRepo: userRepository,
+	}
 }
