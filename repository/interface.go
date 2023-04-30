6a718521c39edcc3ecb5150110e1859e19230026
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-04-30 19:29:49 +0700

    feat: add interface repository

---
+++ repository/interface.go
@@ -0,0 +1,13 @@
+package repository
+
+import (
+	"context"
+	"github.com/betawulan/sahamrakyat/model"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user model.User) (model.User, error)
+	ReadByID(ctx context.Context, ID int64) (model.User, error)
+	Update(ctx context.Context, ID int64, user model.User) error
+	Delete(ctx context.Context, ID int64) error
+}
