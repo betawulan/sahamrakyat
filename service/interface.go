0fe6a9c3d1957a08a7360a867282acff28314a1f
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 16:11:29 +0700

    feat: add interfaces order item at service

--- service/interface.go
+++ service/interface.go
@@ -12,3 +12,10 @@ type UserService interface {
 	Delete(ctx context.Context, ID int64) error
 	Read(ctx context.Context, filter model.UserFilter) (model.UserResponse, error)
 }
+
+type OrderItemService interface {
+	Create(ctx context.Context, orderItem model.OrderItem) (model.OrderItem, error)
+	ReadByID(ctx context.Context, ID int64) (model.OrderItem, error)
+	Update(ctx context.Context, ID int64, orderItem model.OrderItem) error
+	Delete(ctx context.Context, ID int64) error
+}
