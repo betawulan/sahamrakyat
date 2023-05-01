58fb4fea8fd2738dfdc5398cee0b28c56bfd8fd5
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 15:56:44 +0700

    feat: add interface order item at repository

--- repository/interface.go
+++ repository/interface.go
@@ -12,3 +12,10 @@ type UserRepository interface {
 	Delete(ctx context.Context, ID int64) error
 	Read(ctx context.Context, filter model.UserFilter) ([]model.User, error)
 }
+
+type OrderItemRepository interface {
+	Create(ctx context.Context, orderItem model.OrderItem) (model.OrderItem, error)
+	ReadByID(ctx context.Context, ID int64) (model.OrderItem, error)
+	Update(ctx context.Context, ID int64, orderItem model.OrderItem) error
+	Delete(ctx context.Context, ID int64) error
+}
