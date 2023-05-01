c2aba8104884c6b397dd8f19f4ac382990c65b61
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 16:31:23 +0700

    feat: add migration create table orders_item

---
+++ migrations/20230501162940_create_table_order_item.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+create table orders_item (
+    id int not null auto_increment,
+    name varchar(100) not null,
+    price int not null,
+    expired_at timestamp not null,
+    created_at timestamp(3) default current_timestamp(3),
+    updated_at timestamp(3) default null,
+    deleted_at timestamp(3) default null,
+    primary key(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table orders_item;
+-- +goose StatementEnd
