3fa351439997508287cf4deebaf09b74df6c6369
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 10:26:43 +0700

    style: change style json from camel case to snake case

--- model/user.go
+++ model/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID         int64     `json:"id"`
-	FullName   string    `json:"fullName"`
-	FirstOrder string    `json:"firstOrder"`
+	FullName   string    `json:"fullname"`
+	FirstOrder string    `json:"first_order"`
 	CreatedAt  time.Time `json:"-"`
 	UpdatedAt  time.Time `json:"-"`
 	DeletedAt  time.Time `json:"-"`
