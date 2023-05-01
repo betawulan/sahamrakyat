8059ebee1fc0f4b651abdc654b5cef6b9c03b61d
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 16:16:59 +0700

    feat: add function create order item at service

---
+++ service/order_item.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"context"
+
+	"github.com/betawulan/sahamrakyat/model"
+	"github.com/betawulan/sahamrakyat/repository"
+)
+
+type orderItemService struct {
+	orderItemRepo repository.OrderItemRepository
+}
+
+func (o orderItemService) Create(ctx context.Context, orderItem model.OrderItem) (model.OrderItem, error) {
+	orderItem, err := o.orderItemRepo.Create(ctx, orderItem)
+	if err != nil {
+		return model.OrderItem{}, err
+	}
+
+	return orderItem, nil
+}
+
+func (o orderItemService) ReadByID(ctx context.Context, ID int64) (model.OrderItem, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderItemService) Update(ctx context.Context, ID int64, orderItem model.OrderItem) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o orderItemService) Delete(ctx context.Context, ID int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewOrderItemService(orderItemRepository repository.OrderItemRepository) OrderItemService {
+	return orderItemService{
+		orderItemRepo: orderItemRepository,
+	}
+}
