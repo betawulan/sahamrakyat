e55a5a1dc9a1938284c4d6d10afc5659ae25d7c2
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 10:48:58 +0700

    feat: add endpoint read by id of user

--- delivery/user.go
+++ delivery/user.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 
@@ -29,10 +30,32 @@ func (u userDelivery) create(c echo.Context) error {
 	return c.JSON(http.StatusCreated, user)
 }
 
+func (u userDelivery) readByID(c echo.Context) error {
+	var user model.User
+
+	ID := c.Param("id")
+	if ID != "" {
+		IDint, err := strconv.Atoi(ID)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+
+		user.ID = int64(IDint)
+	}
+
+	usr, err := u.userService.ReadByID(c.Request().Context(), user.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, usr)
+}
+
 func RegisterUserRoute(userService service.UserService, e *echo.Echo) {
 	handler := userDelivery{
 		userService: userService,
 	}
 
 	e.POST("/user", handler.create)
+	e.GET("/user/:id", handler.readByID)
 }
