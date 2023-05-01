8ac598726316cca58b1df76e3f643c1e10a39e9f
t-beta.wulandari <t-beta.wulandari@virgoku.id>
2023-05-01 16:42:32 +0700

    fix: change datatype expired_at of order item

--- migrations/20230501162940_create_table_order_item.sql
+++ migrations/20230501162940_create_table_order_item.sql
@@ -4,7 +4,7 @@ create table orders_item (
     id int not null auto_increment,
     name varchar(100) not null,
     price int not null,
-    expired_at timestamp not null,
+    expired_at varchar(20) not null,
     created_at timestamp(3) default current_timestamp(3),
     updated_at timestamp(3) default null,
     deleted_at timestamp(3) default null,
--- model/order_item.go
+++ model/order_item.go
@@ -6,7 +6,7 @@ type OrderItem struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	Price     int64     `json:"price"`
-	ExpiredAt time.Time `json:"expired_at"`
+	ExpiredAt string    `json:"expired_at"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 	DeletedAt time.Time `json:"-"`
